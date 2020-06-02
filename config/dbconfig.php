4fc1db1780c98a1d6884d7073fe91833fe8e05e0
Todor Gyumyushev <yodor1@gmail.com>
2020-06-02 03:00:07 +0300

    fixes

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -2,31 +2,14 @@
 
 $prop = new DBConnectionProperties();
 $prop->driver = "MySQLi";
-// $prop->driver = "PDOMySQL";
 
-$prop->database = "sparkbox_demo";
-$prop->user = "sparkbox";
-$prop->pass = "123456";
-$prop->host = "127.0.0.1";
-$prop->port = "3306";
-$prop->is_pdo = FALSE;
+$prop->database="saturnosoftbiz_demo";
+$prop->user="saturnosoftbiz_demo";
+$prop->pass="OJW]7]2@,h.a";
+$prop->host="localhost";
+$prop->port="3306";
+
 
 $prop->setConnectionName("default");
 DBConnections::addProperties($prop);
-
-// $prop1 = new DBConnectionProperties();
-// $prop1->driver = "MySQLi";
-// 
-// $prop1->database="sparkbox-demo";
-// $prop1->user="demo1";
-// $prop1->pass="d3m0d3m0";
-// $prop1->host="localhost";
-// $prop1->port="3306";
-// 
-// 
-// 
-// $prop1->setConnectionName("mysqli_conn");
-// 
-// DBConnections::addConnection($prop1);
-
 ?>
