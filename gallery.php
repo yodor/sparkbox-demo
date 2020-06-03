5863eb56b0025e08fb84eb686fc35153286d98d1
Todor Gyumyushev <yodor1@gmail.com>
2020-06-04 02:25:02 +0300

    refactoring
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- gallery.php
+++ gallery.php
@@ -13,13 +13,19 @@ $qry = $bean->query();
 $qry->select->order_by = " position ASC ";
 $qry->select->fields()->set($bean->key());
 
+$item = new GalleryTapeItem();
+$item->setPhotoSize(-1,160);
+$item->setBeanClass(get_class($bean));
+
 $gallery_tape = new GalleryTape();
+$gallery_tape->setItemRenderer($item);
+
+$gallery_tape->setName("GalleryTape.1");
+
 $gallery_tape->setCaption("Sample Image Gallery");
 
 $gallery_tape->setIterator($qry);
-$gallery_tape->setName("GalleryTape.1");
-$gallery_tape->setPhotoSize(-1, 160);
-$gallery_tape->setBeanClass(get_class($bean));
+
 $page->startRender();
 
 $gallery_tape->render();
