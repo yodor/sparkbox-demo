6afaddaa4784d7c6eddd96471738822d8428f746
Todor Gyumyushev <yodor1@gmail.com>
2023-01-12 22:04:02 +0200

    updates
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- content/index.php
+++ content/index.php
@@ -1,15 +1,20 @@
 <?php
 include_once("session.php");
-include_once("class/pages/MainPage.php");
+include_once("class/pages/DemoPage.php");
 include_once("components/DynamicPageView.php");
-include_once ("components/TextComponent.php");
+include_once("components/TextComponent.php");
+include_once("beans/DynamicPagesBean.php");
 
-$page = new MainPage();
+$page = new DemoPage();
+
+$cmp = new DynamicPageView();
+$cmp->setBean(new DynamicPagesBean());
+
+$cmp->processInput();
 
 $page->startRender();
 
 echo "<div class='inner'>";
-$cmp = new DynamicPageView();
 $cmp->render();
 echo "</div>";
 
