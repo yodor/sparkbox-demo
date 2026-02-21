e77e2207e23fca50e4f2392e2318aaca24a8c55a
Todor Gyumyushev <yodor1@gmail.com>
2026-02-21 03:16:01 +0200

    refactoring

--- ajax_chunkupload.php
+++ ajax_chunkupload.php
@@ -11,10 +11,10 @@ $form = new InputForm();
 
 
 $input = DataInputFactory::Create(InputType::SESSION_CHUNKED_FILE, "document", "Chunk File Upload Document", 1);
-$chunk_size = (2 * 1024 * 1024);//5MB
-//debug
+
+//debug: but not more than max_upload_filesize - Config::MAX_UPLOAD_FILESIZE
 //$chunk_size = (5 * 1024 );//5KB
-$input->getRenderer()->input()->setAttribute("chunk_size", $chunk_size);
+//$input->getRenderer()->input()->setAttribute("chunk_size", $chunk_size);
 $form->addInput($input);
 
 $form_render = new FormRenderer($form);
