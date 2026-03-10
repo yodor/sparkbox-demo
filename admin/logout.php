2682296b6167e59f070ac9bd69210cbffe982f2f
Todor Gyumyushev <yodor1@gmail.com>
2026-03-10 20:16:00 +0200

    refactoring authentication

--- admin/logout.php
+++ admin/logout.php
@@ -1,13 +1,8 @@
 <?php
-include_once("session.php");
 include_once("auth/AdminAuthenticator.php");
 
 $auth = new AdminAuthenticator();
 $auth->logout();
-Session::Destroy();
-
 
 header("Location: " . Spark::Get(Config::ADMIN_LOCAL)  . "/");
 exit;
-
-?>
